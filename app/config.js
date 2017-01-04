e1d3ccded7f2262e368ca2b4cb5177c6ca22d445
Flavio Castro <flcf@cesar.org.br>
2017-01-04 11:45:10 -0300

    Add server side requisition to restore the gateway to factory settings

--- app/config.js
+++ app/config.js
@@ -1,7 +1,9 @@
 var DEVICES_FILE = process.env.KEYS_FILE || '/etc/knot/keys.json';
 var CONFIGURATION_FILE = process.env.CONFIG_FILE || '/etc/knot/gatewayConfig.json';
+var DEFAULT_CONFIGURATION_FILE = process.env.DEFAULT_CONFIG_FILE || '/etc/knot/gatewayFactoryConfig.json';
 
 module.exports = {
   DEVICES_FILE: DEVICES_FILE,
-  CONFIGURATION_FILE: CONFIGURATION_FILE
+  CONFIGURATION_FILE: CONFIGURATION_FILE,
+  DEFAULT_CONFIGURATION_FILE: DEFAULT_CONFIGURATION_FILE
 };
